490fb01f42307a3d6c62cef8eccd9a1a933b1aec
ge-nau <yogabiyi@gmail.com>
2023-01-02 08:48:38 -0800

    t2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,10 @@
  */
  void print_alphabet(void)
  {
- char letter;
-
- for (letter = 'a'; letter <= 'z'; letter++)
- _putchar(letter);
-
- _putchar('\n');
+	 char x;
+	
+	 for (x = 'a'; x <= 'z'; x++)
+		 _putchar(x);
+	 _putchar('\n');
 
  }
