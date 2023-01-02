d87f4a3eab9b7a9e2c88ecc16a2a38183b5ddab9
ge-nau <yogabiyi@gmail.com>
2023-01-02 11:23:32 -0800

    t9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table -print the 9 times table
+ */
+void time_table(void)
+{
+	int num, times, equals;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (times = 1; times <= 9; times++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			equals = num * times;
+
+			if (equals <= 9)
+				_putchar(' ');
+			else
+				_putchar((equals / 10) + '0');
+
+			_putchar((equals % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
