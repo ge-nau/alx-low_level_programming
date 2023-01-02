fa4de9a7d9a52f6f5651a515ab81eb5d75332c18
ge-nau <yogabiyi@gmail.com>
2023-01-02 11:29:56 -0800

    t10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two intergres
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return: the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 +num2);
+}
