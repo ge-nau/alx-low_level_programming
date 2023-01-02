cdb2bdd3688179cc5d46a31a4e68169791f8263e
ge-nau <yogabiyi@gmail.com>
2023-01-02 09:52:30 -0800

    lowercase, amybe not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase
+ * c: we are checking c
+ *
+ * Return; 1 it character is lowercase if not return 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
