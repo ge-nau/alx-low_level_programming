8ad7a2314ce5b9be05d78fe45895613a9601597a
ge-nau <yogabiyi@gmail.com>
2023-01-04 10:05:35 -0800

    t4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * more_number - prints more nums
+ * Return: 0
+ */
+
+void more_number(void)
+{
+	int i, c;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar((c / 10) + 48);
+			}
+			_putchar((c % 10) + 48);
+		}
+
+		_putchar('\n');
+
+		i++
+	}
+}
