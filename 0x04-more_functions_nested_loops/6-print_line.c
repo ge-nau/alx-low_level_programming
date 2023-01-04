1359be6ec00f2eb93dea0d813ca085a96f15a1d4
ge-nau <yogabiyi@gmail.com>
2023-01-04 10:23:23 -0800

    t5

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ * @n: num of liness
+ *
+ * Return: void
+ */
+
+void print_lines(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}q
