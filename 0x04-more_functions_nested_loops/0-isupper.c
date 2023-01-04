35011a0e76fde328491ad88f251372f1edb55fdd
ge-nau <yogabiyi@gmail.com>
2023-01-04 09:07:20 -0800

    t0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 1 if c is uppercase otherwise return 0
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
